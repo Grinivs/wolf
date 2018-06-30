a0fb6c630ae524b027f9e1434fe6097eb0f898a1
Nicolas Grichting <nicolas.grichting95@gmail.com>
2018-06-30 14:33:38 +0200

    add web.php

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,71 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+		$posts = App\Post::orderBy('id', 'desc')->take(3)->get();
+    return view('welcome', compact('posts'));
 });
+
+Route::get('home', function () {
+	$posts = App\Post::orderBy('id', 'desc')->take(3)->get();
+    return view('welcome', compact('posts'));
+});
+
+
+Route::get('post/{slug}', function($slug){
+	$post = App\Post::where('slug', '=', $slug)->firstOrFail();
+	return view('post', compact('post'));
+});
+
+Route::get('shop', function () {
+    return view('shop');
+});
+Route::get('config', function () {
+    return view('config.php');
+});
+Route::get('contact', function () {
+	return view('contact');
+});
+Route::get('chart', function () {
+	return view('chart');
+});
+Route::get('post', function () {
+	return view('post');
+});
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
+
+Route::group(['middleware' => 'guest'], function() {
+  Route::get('signin', [
+    'uses' => 'UserController@getSignin',
+    'as' => 'signin'
+  ]);
+
+  Route::post('signin', [
+    'uses' => 'UserController@postSignin',
+    'as' => 'signin'
+  ]);
+});
+
+
+ Route::get('profile', function () {
+	return view('profile');
+});
+ Route::get('profile', [
+  'uses' => 'UserController@getProfile',
+  'as' => 'profile',
+  'middleware' => 'auth'
+]);
+
+Route::get('logout', [
+  'uses' => 'UserController@getLogout',
+  'as' => 'logout',
+  'middleware' => 'auth'
+]);
+
+Route::post('logout', [
+  'uses' => 'UserController@getLogout',
+  'as' => 'logout',
+  'middleware' => 'auth'
+]);
