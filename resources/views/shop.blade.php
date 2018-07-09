1cab943a1dd80d88fa833c904942e0dae4c3e822
Nicolas Grichting <nicolas.grichting95@gmail.com>
2018-07-09 21:00:53 +0200

    l

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.layout')
 @section('content')
-<?php require_once(public_path('/public/config.php')); ?>
+<?php require_once(public_path('config.php')); ?>
 <!-- ##### Course Area Start ##### -->
     <div class="cryptos-feature-area section-padding-100-0">
         <div class="container-fluid">
